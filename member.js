cfe9b27eea7806b25f24a14afbe4d96eafaeeb7a
elcalafate <42738251+elcalafate@users.noreply.github.com>
2023-07-23 14:54:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: skills
+    };
+    // Write code here
+    return member.skills[3];
+}
